bf903e0f4c693fd95d515ad63a454dc2d46a38d2
Eduardo Aguilar <dante.aguilar41@gmail.com>
2016-12-18 23:34:35 -0600

    bin 2

--- bin/install_corephp.php
+++ bin/install_corephp.php
@@ -1,4 +1,14 @@
 <?php
 
-require_once __DIR__ . "/../autoload.php";
-require_once __DIR__ . '/../corephp/mapper/Installer/Install.php';
+$autoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+    __DIR__ . '/../corephp/mapper/Installer/Install.php',
+    __DIR__ . '/../../../corephp/mapper/Installer/Install.php'
+];
+
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        require_once $autoloadFile;
+    }
+}
